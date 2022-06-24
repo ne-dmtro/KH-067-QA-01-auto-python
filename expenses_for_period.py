6bbeb5066bcb06085481464e081e11a0d6234e17
irenebell <irinaselen1@gmail.com>
2022-06-24 13:04:10 +0300

    changed expenses_for_period.py

--- expenses_for_period.py
+++ expenses_for_period.py
@@ -1,12 +1,32 @@
 import csv
-import date_input_exceptions
+import re
 
 
 def get_expenses_for_period():
-    first_date = input("Enter the first date in format 'YYYY-MM-DD': ")
-    date_input_exceptions.check_input(first_date)
-    second_date = input("Enter the second date in format 'YYYY-MM-DD': ")
-    date_input_exceptions.check_input(second_date)
+    correct_first_date = False
+    first_date = ''
+    while correct_first_date == False:
+        first_date = input("Enter the first date in format 'YYYY-MM-DD': ")
+        try:
+            if re.match(r'(\d{4}-\d{2}-\d{2})', first_date):
+                correct_first_date = True
+            else:
+                raise ValueError("Error: Date format invalid.")
+        except ValueError:
+            print("Error: Date format invalid.")
+
+    correct_second_date = False
+    second_date = ''
+    while correct_second_date == False:
+        second_date = input("Enter the second date in format 'YYYY-MM-DD': ")
+        try:
+            if re.match(r'(\d{4}-\d{2}-\d{2})', first_date):
+                correct_second_date = True
+            else:
+                raise ValueError("Error: Date format invalid.")
+        except ValueError:
+            print("Error: Date format invalid.")
+
     with open("expenses.csv") as fin:
         total = 0
         for row in csv.reader(fin):
@@ -14,4 +34,5 @@ def get_expenses_for_period():
                 total += float(row[2])
         print(f'Your expenses for this period = {total}')
 
-get_expenses_for_period()
+
+get_expenses_for_period()()
