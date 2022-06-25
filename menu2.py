de1bab1c285e5361035ba2489c6d3a66586d7613
ne-dmtro <ne-dmtro@protonmail.com>
2022-06-25 15:07:42 +0300

    Deleted all testing menu files

--- menu2.py
+++
@@ -1,9 +0,0 @@
-import enquiries
-
-
-options = ['thing 1', 'thing 2', 'thing 3']
-choice = enquiries.choose('Choose one of these options: ', options)
-
-if enquiries.confirm('Do you want to write something?'):
-    text = enquiries.freetext('Write something interesting: ')
-    print(text)
